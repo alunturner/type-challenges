67d6f9f2fe0793751dbbe089056b310466fb9abd
alunturner <alun.turner@googlemail.com>
2023-07-31 18:01:42 +0100

    medium tuple to union

--- playground/medium/00010-medium-tuple-to-union.ts
+++ playground/medium/00010-medium-tuple-to-union.ts
@@ -20,7 +20,9 @@
 
 /* _____________ Your Code Here _____________ */
 
-type TupleToUnion<T> = any
+type TupleToUnion<T> = T extends [infer F, ...infer R]
+  ? F | TupleToUnion<R>
+  : never
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
