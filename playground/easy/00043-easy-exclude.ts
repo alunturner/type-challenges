48c78e32611c16bb5133db46f8c95afb1f8879a4
Alun Turner <alun.turner@googlemail.com>
2023-07-31 08:52:02 +0100

    easy exclude

--- playground/easy/00043-easy-exclude.ts
+++ playground/easy/00043-easy-exclude.ts
@@ -20,15 +20,18 @@
 
 /* _____________ Your Code Here _____________ */
 
-type MyExclude<T, U> = any
+type MyExclude<T, U> = T extends U ? never : T
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
 
+type thing = MyExclude<'hi' | 'there', 'hi'>
 type cases = [
   Expect<Equal<MyExclude<'a' | 'b' | 'c', 'a'>, 'b' | 'c'>>,
   Expect<Equal<MyExclude<'a' | 'b' | 'c', 'a' | 'b'>, 'c'>>,
-  Expect<Equal<MyExclude<string | number | (() => void), Function>, string | number>>,
+  Expect<
+    Equal<MyExclude<string | number | (() => void), Function>, string | number>
+  >,
 ]
 
 /* _____________ Further Steps _____________ */
