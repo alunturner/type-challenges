b7c6b5e7c4daf862edd7a108ab40ae61049f1ddd
Alun Turner <alun.turner@googlemail.com>
2023-07-31 08:33:51 +0100

    easy readonly

--- playground/easy/00007-easy-readonly.ts
+++ playground/easy/00007-easy-readonly.ts
@@ -31,14 +31,15 @@
 
 /* _____________ Your Code Here _____________ */
 
-type MyReadonly<T> = any
+type MyReadonly<T> = {
+  readonly [K in keyof T]: T[K]
+}
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
 
-type cases = [
-  Expect<Equal<MyReadonly<Todo1>, Readonly<Todo1>>>,
-]
+type thing = MyReadonly<{ one: 'two'; three: 'four'; five: { six: 'seven' } }>
+type cases = [Expect<Equal<MyReadonly<Todo1>, Readonly<Todo1>>>]
 
 interface Todo1 {
   title: string
