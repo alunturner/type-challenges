c2a56a6d5cb2cf8347aaca51ce7708a80e77f787
Alun Turner <alun.turner@googlemail.com>
2023-07-31 09:19:24 +0100

    easy concat

--- playground/easy/00533-easy-concat.ts
+++ playground/easy/00533-easy-concat.ts
@@ -18,19 +18,33 @@
 
 /* _____________ Your Code Here _____________ */
 
-type Concat<T, U> = any
+type Concat<T, U, Acc extends any[] = []> = T extends readonly [
+  infer Ft,
+  ...infer Rt,
+]
+  ? Concat<Rt, U, [...Acc, Ft]>
+  : U extends readonly [infer Fu, ...infer Ru]
+  ? Concat<T, Ru, [...Acc, Fu]>
+  : Acc
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
 
 const tuple = [1] as const
+type tuple = readonly [1]
+type thing = Concat<tuple, tuple>
 
 type cases = [
   Expect<Equal<Concat<[], []>, []>>,
   Expect<Equal<Concat<[], [1]>, [1]>>,
   Expect<Equal<Concat<typeof tuple, typeof tuple>, [1, 1]>>,
   Expect<Equal<Concat<[1, 2], [3, 4]>, [1, 2, 3, 4]>>,
-  Expect<Equal<Concat<['1', 2, '3'], [false, boolean, '4']>, ['1', 2, '3', false, boolean, '4']>>,
+  Expect<
+    Equal<
+      Concat<['1', 2, '3'], [false, boolean, '4']>,
+      ['1', 2, '3', false, boolean, '4']
+    >
+  >,
 ]
 
 // @ts-expect-error
