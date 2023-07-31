54cbe1f533b8759e2503b51c6e5e18d0b74d8d0d
alunturner <alun.turner@googlemail.com>
2023-07-31 14:14:37 +0100

    medium omit

--- playground/medium/00003-medium-omit.ts
+++ playground/medium/00003-medium-omit.ts
@@ -30,7 +30,9 @@
 
 /* _____________ Your Code Here _____________ */
 
-type MyOmit<T, K> = any
+type MyOmit<T, K> = {
+  [key in keyof T as key extends K ? never : key]: T[key]
+}
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
