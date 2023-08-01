5069ac0888d35e5f63a3af7e257d1ccb9e3abe92
Alun Turner <alun.turner@googlemail.com>
2023-08-01 16:51:20 +0100

    medium capitalize

--- playground/medium/00110-medium-capitalize.ts
+++ playground/medium/00110-medium-capitalize.ts
@@ -17,8 +17,40 @@
 */
 
 /* _____________ Your Code Here _____________ */
+type LetterMap = {
+  a: 'A'
+  b: 'B'
+  c: 'C'
+  d: 'D'
+  e: 'E'
+  f: 'F'
+  g: 'G'
+  h: 'H'
+  i: 'I'
+  j: 'J'
+  k: 'K'
+  l: 'L'
+  m: 'M'
+  n: 'N'
+  o: 'O'
+  p: 'P'
+  q: 'Q'
+  r: 'R'
+  s: 'S'
+  t: 'T'
+  u: 'U'
+  v: 'V'
+  w: 'W'
+  x: 'X'
+  y: 'Y'
+  z: 'Z'
+}
 
-type MyCapitalize<S extends string> = any
+type MyCapitalize<S extends string> = S extends `${infer F}${infer R}`
+  ? F extends keyof LetterMap
+    ? `${LetterMap[F]}${R}`
+    : S
+  : S
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
