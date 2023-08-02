af3fd76a67eb5f6ed8e037a79f0861686c2a4301
Alun Turner <alun.turner@googlemail.com>
2023-08-02 12:00:29 +0100

    medium flatten

--- playground/medium/00459-medium-flatten.ts
+++ playground/medium/00459-medium-flatten.ts
@@ -18,17 +18,27 @@
 
 /* _____________ Your Code Here _____________ */
 
-type Flatten = any
+type Flatten<T> = T extends []
+  ? []
+  : T extends [infer F extends any, ...infer R]
+  ? [...Flatten<F>, ...Flatten<R>]
+  : [T]
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
 
+type thing = Flatten<[1, 2, [3, 4], [[[5]]]]>
 type cases = [
   Expect<Equal<Flatten<[]>, []>>,
   Expect<Equal<Flatten<[1, 2, 3, 4]>, [1, 2, 3, 4]>>,
   Expect<Equal<Flatten<[1, [2]]>, [1, 2]>>,
   Expect<Equal<Flatten<[1, 2, [3, 4], [[[5]]]]>, [1, 2, 3, 4, 5]>>,
-  Expect<Equal<Flatten<[{ foo: 'bar'; 2: 10 }, 'foobar']>, [{ foo: 'bar'; 2: 10 }, 'foobar']>>,
+  Expect<
+    Equal<
+      Flatten<[{ foo: 'bar'; 2: 10 }, 'foobar']>,
+      [{ foo: 'bar'; 2: 10 }, 'foobar']
+    >
+  >,
 ]
 
 // @ts-expect-error
