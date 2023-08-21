48fc5c14cb37c78cda856e3f3a966ad4f3b24ba9
Alun Turner <alun.turner@googlemail.com>
2023-08-21 08:30:30 +0100

    medium flip

--- playground/medium/04179-medium-flip.ts
+++ playground/medium/04179-medium-flip.ts
@@ -20,16 +20,21 @@
 
 /* _____________ Your Code Here _____________ */
 
-type Flip<T> = any
+type KeyType = string | number | boolean
+type Flip<T extends Record<Exclude<KeyType, boolean>, KeyType>> = {
+  [key in keyof T as `${T[key]}`]: key
+}
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect, NotEqual } from '@type-challenges/utils'
-
+type thing = Flip<{ pi: 3.14; bool: true }>
 type cases = [
   Expect<Equal<{ a: 'pi' }, Flip<{ pi: 'a' }>>>,
   Expect<NotEqual<{ b: 'pi' }, Flip<{ pi: 'a' }>>>,
   Expect<Equal<{ 3.14: 'pi'; true: 'bool' }, Flip<{ pi: 3.14; bool: true }>>>,
-  Expect<Equal<{ val2: 'prop2'; val: 'prop' }, Flip<{ prop: 'val'; prop2: 'val2' }>>>,
+  Expect<
+    Equal<{ val2: 'prop2'; val: 'prop' }, Flip<{ prop: 'val'; prop2: 'val2' }>>
+  >,
 ]
 
 /* _____________ Further Steps _____________ */
