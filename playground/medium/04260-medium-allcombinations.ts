4b4f9fbf5caea72c4924986a330820824c7e4362
Alun Turner <alun.turner@googlemail.com>
2023-08-22 08:16:15 +0100

    medium all combinations

--- playground/medium/04260-medium-allcombinations.ts
+++ playground/medium/04260-medium-allcombinations.ts
@@ -19,7 +19,15 @@
 
 /* _____________ Your Code Here _____________ */
 
-type AllCombinations<S> = any
+type StringToUnion<S extends string> = S extends `${infer F}${infer R}`
+  ? F | StringToUnion<R>
+  : never
+
+type AllCombinations<S extends string, U extends string = StringToUnion<S>> = [
+  U,
+] extends [never]
+  ? ''
+  : '' | { [P in U]: `${P}${AllCombinations<Exclude<U, P>>}` }[U]
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
@@ -28,8 +36,97 @@ type cases = [
   Expect<Equal<AllCombinations<''>, ''>>,
   Expect<Equal<AllCombinations<'A'>, '' | 'A'>>,
   Expect<Equal<AllCombinations<'AB'>, '' | 'A' | 'B' | 'AB' | 'BA'>>,
-  Expect<Equal<AllCombinations<'ABC'>, '' | 'A' | 'B' | 'C' | 'AB' | 'AC' | 'BA' | 'BC' | 'CA' | 'CB' | 'ABC' | 'ACB' | 'BAC' | 'BCA' | 'CAB' | 'CBA'>>,
-  Expect<Equal<AllCombinations<'ABCD'>, '' | 'A' | 'B' | 'C' | 'D' | 'AB' | 'AC' | 'AD' | 'BA' | 'BC' | 'BD' | 'CA' | 'CB' | 'CD' | 'DA' | 'DB' | 'DC' | 'ABC' | 'ABD' | 'ACB' | 'ACD' | 'ADB' | 'ADC' | 'BAC' | 'BAD' | 'BCA' | 'BCD' | 'BDA' | 'BDC' | 'CAB' | 'CAD' | 'CBA' | 'CBD' | 'CDA' | 'CDB' | 'DAB' | 'DAC' | 'DBA' | 'DBC' | 'DCA' | 'DCB' | 'ABCD' | 'ABDC' | 'ACBD' | 'ACDB' | 'ADBC' | 'ADCB' | 'BACD' | 'BADC' | 'BCAD' | 'BCDA' | 'BDAC' | 'BDCA' | 'CABD' | 'CADB' | 'CBAD' | 'CBDA' | 'CDAB' | 'CDBA' | 'DABC' | 'DACB' | 'DBAC' | 'DBCA' | 'DCAB' | 'DCBA'>>,
+  Expect<
+    Equal<
+      AllCombinations<'ABC'>,
+      | ''
+      | 'A'
+      | 'B'
+      | 'C'
+      | 'AB'
+      | 'AC'
+      | 'BA'
+      | 'BC'
+      | 'CA'
+      | 'CB'
+      | 'ABC'
+      | 'ACB'
+      | 'BAC'
+      | 'BCA'
+      | 'CAB'
+      | 'CBA'
+    >
+  >,
+  Expect<
+    Equal<
+      AllCombinations<'ABCD'>,
+      | ''
+      | 'A'
+      | 'B'
+      | 'C'
+      | 'D'
+      | 'AB'
+      | 'AC'
+      | 'AD'
+      | 'BA'
+      | 'BC'
+      | 'BD'
+      | 'CA'
+      | 'CB'
+      | 'CD'
+      | 'DA'
+      | 'DB'
+      | 'DC'
+      | 'ABC'
+      | 'ABD'
+      | 'ACB'
+      | 'ACD'
+      | 'ADB'
+      | 'ADC'
+      | 'BAC'
+      | 'BAD'
+      | 'BCA'
+      | 'BCD'
+      | 'BDA'
+      | 'BDC'
+      | 'CAB'
+      | 'CAD'
+      | 'CBA'
+      | 'CBD'
+      | 'CDA'
+      | 'CDB'
+      | 'DAB'
+      | 'DAC'
+      | 'DBA'
+      | 'DBC'
+      | 'DCA'
+      | 'DCB'
+      | 'ABCD'
+      | 'ABDC'
+      | 'ACBD'
+      | 'ACDB'
+      | 'ADBC'
+      | 'ADCB'
+      | 'BACD'
+      | 'BADC'
+      | 'BCAD'
+      | 'BCDA'
+      | 'BDAC'
+      | 'BDCA'
+      | 'CABD'
+      | 'CADB'
+      | 'CBAD'
+      | 'CBDA'
+      | 'CDAB'
+      | 'CDBA'
+      | 'DABC'
+      | 'DACB'
+      | 'DBAC'
+      | 'DBCA'
+      | 'DCAB'
+      | 'DCBA'
+    >
+  >,
 ]
 
 /* _____________ Further Steps _____________ */
