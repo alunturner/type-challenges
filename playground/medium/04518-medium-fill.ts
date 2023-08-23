213767990fc18559f0209bee0db737594d154c28
alunturner <alun.turner@googlemail.com>
2023-08-23 09:56:06 +0100

    medium fill

--- playground/medium/04518-medium-fill.ts
+++ playground/medium/04518-medium-fill.ts
@@ -24,9 +24,18 @@ type Fill<
   N,
   Start extends number = 0,
   End extends number = T['length'],
-> = any
+  Count extends any[] = [],
+  Flag extends boolean = Count['length'] extends Start ? true : false,
+> = Count['length'] extends End
+  ? T
+  : T extends [infer F, ...infer R]
+    ? Flag extends false
+      ? [F, ...Fill<R, N, Start, End, [...Count, 0]>]
+      : [N, ...Fill<R, N, Start, End, [...Count, 0], Flag>]
+    : T
 
 /* _____________ Test Cases _____________ */
+
 import type { Equal, Expect } from '@type-challenges/utils'
 
 type cases = [
