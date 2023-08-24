5bca6f19c5bce7a3b06d1aa4cc639142fa6db628
alunturner <alun.turner@googlemail.com>
2023-08-24 09:42:18 +0100

    medium construct tuple

--- playground/medium/07544-medium-construct-tuple.ts
+++ playground/medium/07544-medium-construct-tuple.ts
@@ -18,7 +18,9 @@
 
 /* _____________ Your Code Here _____________ */
 
-type ConstructTuple<L extends number> = any
+type ConstructTuple<L extends number, Acc extends unknown[] = []> = Acc['length'] extends L
+  ? Acc
+  : ConstructTuple<L, [...Acc, unknown]>
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
