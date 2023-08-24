705dff5e3ca2bd072733e84769d02d22d90e63db
alunturner <alun.turner@googlemail.com>
2023-08-24 09:30:36 +0100

    medium unique

--- playground/medium/05360-medium-unique.ts
+++ playground/medium/05360-medium-unique.ts
@@ -10,8 +10,8 @@
   ```ts
   type Res = Unique<[1, 1, 2, 2, 3, 3]>; // expected to be [1, 2, 3]
   type Res1 = Unique<[1, 2, 3, 4, 4, 5, 6, 7]>; // expected to be [1, 2, 3, 4, 5, 6, 7]
-  type Res2 = Unique<[1, "a", 2, "b", 2, "a"]>; // expected to be [1, "a", 2, "b"]
-  type Res3 = Unique<[string, number, 1, "a", 1, string, 2, "b", 2, number]>; // expected to be [string, number, 1, "a", 2, "b"]
+  type Res2 = Unique<[1, 'a', 2, 'b', 2, 'a']>; // expected to be [1, 'a', 2, 'b']
+  type Res3 = Unique<[string, number, 1, 'a', 1, string, 2, 'b', 2, number]>; // expected to be [string, number, 1, 'a', 2, 'b']
   type Res4 = Unique<[unknown, unknown, any, any, never, never]>; // expected to be [unknown, any, never]
   ```
 
@@ -20,17 +20,36 @@
 
 /* _____________ Your Code Here _____________ */
 
-type Unique<T> = any
+type Includes<T, U> = U extends [infer F, ...infer Rest]
+  ? Equal<F, T> extends true
+    ? true
+    : Includes<T, Rest>
+  : false
+
+type Unique<T, U extends any[] = []> = T extends [infer R, ...infer F]
+  ? Includes<R, U> extends true
+    ? Unique<F, [...U]>
+    : Unique<F, [...U, R]>
+  : U
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
-
 type cases = [
   Expect<Equal<Unique<[1, 1, 2, 2, 3, 3]>, [1, 2, 3]>>,
   Expect<Equal<Unique<[1, 2, 3, 4, 4, 5, 6, 7]>, [1, 2, 3, 4, 5, 6, 7]>>,
   Expect<Equal<Unique<[1, 'a', 2, 'b', 2, 'a']>, [1, 'a', 2, 'b']>>,
-  Expect<Equal<Unique<[string, number, 1, 'a', 1, string, 2, 'b', 2, number]>, [string, number, 1, 'a', 2, 'b']>>,
-  Expect<Equal<Unique<[unknown, unknown, any, any, never, never]>, [unknown, any, never]>>,
+  Expect<
+      Equal<
+          Unique<[string, number, 1, 'a', 1, string, 2, 'b', 2, number]>,
+          [string, number, 1, 'a', 2, 'b']
+      >
+  >,
+  Expect<
+      Equal<
+          Unique<[unknown, unknown, any, any, never, never]>,
+          [unknown, any, never]
+      >
+  >,
 ]
 
 /* _____________ Further Steps _____________ */
